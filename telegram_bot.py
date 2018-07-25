e605c020fc329aecc9b1e0493b2cb9e9e550545c
Tom0001 <pavshievoin@gmail.com>
2018-07-26 02:35:30 +0300

    Code optimization

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,19 +6,10 @@ import schedule
 from time import sleep
 # from datetime import datetime
 
-url = "https://api.telegram.org/bot615432346:AAF5DadZtgo8isAWdNyXaC3oy3QtzAjwphE/"
+# data = "sendMessage?chat_id=561706344&text="
 # messages = ["Good morning", "Good afternoon",
 # 			"Good evening", "Good night"]
-data = "sendMessage?chat_id=561706344&text="
-# os.system("sudo systemctl start tor")
-# time.sleep(10)
-# def get_tor_session():
-# 	session = requests.session()
-# 	session.proxies = {"http" : "socks5://127.0.0.1:9050",
-# 						"https": "socks5://127.0.0.1:9050"}
-# 	return session
 
-# session = get_tor_session()
 # class BotHandler():
 
 # 	def __init__(self, token):
@@ -55,61 +46,45 @@ data = "sendMessage?chat_id=561706344&text="
 # 	responce = requests.post(url + data + text)
 # # greet_bot = BotHandler(token)  
 
-def good_morning():
-	print("Good morning is working!")
-	responce = requests.post(url + data + "Good morning!")
+# def get_updates():
+# 	responce = requests.get(url + token + "/getUpdates")
+# 	return responce.json()["result"]
 
-def good_afternoon():
-	print("Good afternoon function is working!")
-	responce = requests.post(url + data + "Good afternoon!")
+def greetings(time):
+	if time == "03:00":
+		send_message(dev_chat_id, "Good morning!")
+	elif time == "09:00":
+		send_message(dev_chat_id, "Good afternoon!")
+	elif time == "15:00":
+		send_message(dev_chat_id, "Good evening!")
+	else:
+		send_message(dev_chat_id, "Good night!")
 
-def good_evening():
-	print("Good evening function is working")
-	responce = requests.post(url + data + "Good evening!")
-
-def good_night():
-	print("Good night function is working!")
-	responce = requests.post(url + data + "Good night!")
-
-schedule.every().day.at("03:00").do(good_morning)
-schedule.every().day.at("09:00").do(good_afternoon)
-schedule.every().day.at("15:00").do(good_evening)
-schedule.every().day.at("21:00").do(good_night)
+def send_message(chat_id, text):
+	bot_url += "sendMessage?chat_id={}&text={}".format(
+		chat_id, text)
+	requests.post(bot_url)
 
 def main():
+	schedule.every().day.at("03:00").do(greetings, "03:00")
+	schedule.every().day.at("09:00").do(greetings, "09:00")
+	schedule.every().day.at("15:00").do(greetings, "15:00")
+	schedule.every().day.at("21:00").do(greetings, "21:00")
+
 	while True:
 		try:
 			schedule.run_pending()
 			sleep(60)
 		except Exception:
 			print(Exception)
-			requests.post(url + data + Exception)
+			send_message(dev_chat_id, Exception)
 			exit()
-#     new_offset = None
 
-#     while True:
-#         greet_bot.get_updates(new_offset)
 
-#         last_update = greet_bot.get_last_update()
-
-#         last_update_id = last_update['update_id']
-#         last_chat_text = last_update['message']['text']
-#         last_chat_id = last_update['message']['chat']['id']
-#         last_chat_name = last_update['message']['chat']['first_name']
-
-#         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-#             greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-#             today += 1
-
-#         elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-#             greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-#             today += 1
-
-#         elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-#             greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-#             today += 1
-
-#         new_offset = last_update_id + 1
+site = "https://api.telegram.org/bot"
+token = "615432346:AAF5DadZtgo8isAWdNyXaC3oy3QtzAjwphE"
+bot_url = site + token + '/'
+dev_chat_id = "561706344"
 
 if __name__ == '__main__':
 	main()
